382543399764c5c1928caaab38b581e60cffd40d
Ilango <ilangokul@gmail.com>
2018-05-25 23:46:27 +0530

    cifar10 import train

--- cifar.py
+++ cifar.py
@@ -1,10 +1,30 @@
 import os
 import tarfile
 
+import numpy as np
 import requests
 
 __cifar_10_url = "http://www.cs.utoronto.ca/%7Ekriz/cifar-10-binary.tar.gz"
 __cifar_100_url = "http://www.cs.utoronto.ca/%7Ekriz/cifar-100-binary.tar.gz"
+
+cifar10_dir = "cifar-10-batches-bin"
+
+
+def cifar10_train():
+    data_batch = os.path.join(cifar10_dir, "data_batch_%d.bin")
+    trlab = []
+    trim = []
+
+    for i in range(1, 6):
+        with open(data_batch % i, "rb") as f:
+            for _ in range(10000):
+                trlab.append(int.from_bytes(f.read(1), 'big'))
+                trim.append(list(f.read(3072)))
+    trlab = np.array(trlab)
+    trim = np.array(trim)
+    trim.shape = (50000, 3, 32, 32)
+    trim = trim.transpose([0, 2, 3, 1])
+    return trim, trlab
 
 
 def prepare_cifar_10():
